f89c2ed1c50795e86a171d16749137b13fdb8198
John Erickson <ericksonjohn08@hotmail.com>
2016-12-30 10:20:43 -0500

    Added image for User Experience, link functionality to most social icons and Tic Tac Toe

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+// ==============================================
+// portfolio | img hover attributes
+// ==============================================
+$("#tic_tac_toe").attr("data-content", "Tic Tac Toe");
+$("#omnifood").attr("data-content", "Omnifood");
+$("#user_experience").attr("data-content", "User Experience");
