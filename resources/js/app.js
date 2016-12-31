d88f0aba5cb526d2bb5f599409ad2668f4302562
John Erickson <ericksonjohn08@hotmail.com>
2016-12-31 11:20:44 -0500

    Added website appropriate colors for social icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
-// ==============================================
-// portfolio | img hover attributes
-// ==============================================
-$("#tic_tac_toe").attr("data-content", "Tic Tac Toe");
-$("#omnifood").attr("data-content", "Omnifood");
-$("#user_experience").attr("data-content", "User Experience");
+$(document).ready(function() {
+	$("#tic_tac_toe").attr("data-content", "Tic Tac Toe");
+	$("#omnifood").attr("data-content", "Omnifood");
+	$("#user_experience").attr("data-content", "User Experience");
+});
