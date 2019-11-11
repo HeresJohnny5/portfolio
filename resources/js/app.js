dc55dcc4bb567d0cd7183c2f51da150c171e3bad
HeresJohnny5 <ericksonjohn01@hotmail.com>
2019-11-10 23:32:34 -0500

    Add Animal Rescue work in progress.

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,37 +2,38 @@
 /*global $, jQuery, alert*/
 
 $(document).ready(function () {
-	"use strict";
+  "use strict";
 
-	/* Text hover animation on portfolio images */
-	$("#enterprise").attr("data-content", "Enterprise Mockup");
-	$("#tic_tac_toe").attr("data-content", "Tic Tac Toe");
-	$("#omnifood").attr("data-content", "Omnifood");
-	$("#user_experience").attr("data-content", "User Experience");
-	$("#color_guessing_game").attr("data-content", "Color Guessing Game");
-	$("#todo_list").attr("data-content", "To-Do List");
+  /* Text hover animation on portfolio images */
+  $("#animal-rescue").attr("data-content", "Animal Rescue");
+  $("#enterprise").attr("data-content", "Enterprise Mockup");
+  $("#tic_tac_toe").attr("data-content", "Tic Tac Toe");
+  $("#omnifood").attr("data-content", "Omnifood");
+  $("#user_experience").attr("data-content", "User Experience");
+  $("#color_guessing_game").attr("data-content", "Color Guessing Game");
+  $("#todo_list").attr("data-content", "To-Do List");
 
-	/* Navigation Scroll */
-	$(function () {
-		$('a[href*="#"]:not([href="#"])').click(function () {
-			if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-				if (target.length) {
-					$('html, body').animate({
-						scrollTop: target.offset().top
-					}, 1000);
-					return false;
-				}
-			}
-		});
-	});
+  /* Navigation Scroll */
+  $(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
-	/* Animation on Scroll */
-	$('.js--wp-1').waypoint(function (direction) {
-		$('.js--wp-1').addClass('animated fadeInUp');
-	}, {
-		offset: '50%'
-	});
+  /* Animation on Scroll */
+  $('.js--wp-1').waypoint(function (direction) {
+    $('.js--wp-1').addClass('animated fadeInUp');
+  }, {
+      offset: '50%'
+    });
 
 });
